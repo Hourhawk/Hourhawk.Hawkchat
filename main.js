bd79d2310bfe5b177037f005e8c1b51e1eb1141e
Hourhawk <ryanhuellen@icloud.com>
2018-10-06 15:32:20 -0500

    Added Login & Cookies + Fixed MySQL Injection

--- main.js
+++ main.js
@@ -48,39 +48,111 @@ function mysql_escape (str) {
 }
 
 var sockets = {};
+var loggedUsers = {};
 io.on('connection', function(socket) {
   /* clientId */
-  var clientId = shortid.generate();
-  sockets[clientId] = {loggedin: false};
+  var clientId = shortid.generate() + shortid.generate() + shortid.generate();
+  sockets[clientId] = {loggedIn: false, cookies:{}};
   socket.emit('clientId', clientId);
   /* clientId */
   /* signup-btn */
   socket.on('signup-btn', function($data) {
+    if(sockets[clientId]['loggedIn']) {
+      socket.emit('form-response', {form:'signup-error', message:'You have created the maximum number of accounts.'});
+      return;
+    }
     if($data['email'] == "" || $data['password'] == "" || $data['affiliation'] == "") {
-      socket.emit('form-response', {form:'signup-btn', message:'Please fill in all the fields.'});
+      socket.emit('form-response', {form:'signup-error', message:'Please fill in all the fields.'});
       return;
     }
     if(!validator.validate($data['email'])) {
-      socket.emit('form-response', {form:'signup-btn', message:'Please input a proper email so your account can be validated.'});
+      socket.emit('form-response', {form:'signup-error', message:'Please input a proper email so your account can be validated.'});
       return;
     }
     if($data['password'].length < 7) {
-      socket.emit('form-response', {form:'signup-btn', message:'Your password must be at least seven characters long.'});
+      socket.emit('form-response', {form:'signup-error', message:'Your password must be at least seven characters long.'});
       return;
     }
-    bcrypt.hash($data['password'], 10, function(err, hash) {
+    connection.query("SELECT * FROM users WHERE email='" + mysql_escape($data['email']) + "'", function(err, results, fields) {
       if(err) {
-        socket.emit('form-response', {form:'signup-btn', message:'There was a hashing error. Try again later.'});
+        socket.emit('form-response', {form:'signup-error', message:'There was a database error. Try again later.'});
         return;
       }
-      connection.query("INSERT INTO users(email, password, affiliation) VALUES('" + $data['email'] + "','" + hash + "','" + $data['affiliation'] + "')", function(err, results, fields) {
+      if(results[0] != null) {
+        socket.emit('form-response', {form:'signup-error', message:'Please use a different email. The one entered is already associated with an account.'});
+        return;
+      }
+      bcrypt.hash($data['password'], 10, function(err, hash) {
         if(err) {
-          socket.emit('form-response', {form:'signup-btn', message:'There was a database error. Try again later.'});
+          socket.emit('form-response', {form:'signup-error', message:'There was a hashing error. Try again later.'});
           return;
         }
-        socket.emit('form-response', {form:'signup-btn', message:'You have succesfully signed up. You may now login.'});
+        connection.query("INSERT INTO users(email, password, affiliation) VALUES('" + mysql_escape($data['email']) + "','" + hash + "','" + mysql_escape($data['affiliation']) + "')", function(err, results, fields) {
+          if(err) {
+            socket.emit('form-response', {form:'signup-error', message:'There was a database error. Try again later.'});
+            return;
+          }
+          connection.query("SELECT * FROM users WHERE email='" + mysql_escape($data['email']) + "'", function(err, results, fields) {
+            if(err) {
+              socket.emit('form-response', {form:'signup-error', message:'Your account was created but we had trouble logging you in. Please login manually.'});
+              return;
+            }
+            sockets[clientId]['cookies']['hawkchat-loggedin-identifier'] = shortid.generate() + shortid.generate() + shortid.generate();
+            sockets[clientId]['loggedIn'] = true;
+            socket.emit('cookie-create', sockets[clientId]['cookies']);
+            results[0]['password'] = null;
+            loggedUsers[sockets[clientId]['cookies']['hawkchat-loggedin-identifier']] = results[0];
+            socket.emit('form-response', {form:'signup-error', message:'You have succesfully signed up. Navigate to your web panel.'});
+          });
+        });
+      });
+    }); 
+  });
+  /* signup-btn */
+  /* verify-hawkchat-loggedin-identifier */
+  socket.on('verify-hawkchat-loggedin-identifier', function($loggedInIdentifier) {
+    if(loggedUsers[$loggedInIdentifier] != null) {
+      sockets[clientId]['loggedIn'] = true;
+      sockets[clientId]['cookies']['hawkchat-loggedin-identifier'] = $loggedInIdentifier;
+      socket.emit('verify-hawkchat-loggedin-identifier', loggedUsers[$loggedInIdentifier]);
+    } else {
+      socket.emit('verify-hawkchat-loggedin-identifier');
+    }
+  });
+  /* verify-hawkchat-loggedin-identifier */
+  /* login-btn */
+  socket.on('login-btn', function($data) {
+    if($data['email'] == "" || $data['password'] == "") {
+      socket.emit('form-response', {form:'login-error', message:'Please fill in all the fields.'});
+      return;
+    }
+    connection.query("SELECT * FROM users WHERE email='" + mysql_escape($data['email']) + "'", function(err, results, fields) {
+      if(err) {
+        socket.emit('form-response', {form:'login-error', message:'There was a database error. Try again later.'});
+        return;
+      }
+      if(results[0] == null) {
+        socket.emit('form-response', {form:'login-error', message:'No account is associated with that email.'});
+        return;
+      }
+      bcrypt.compare($data['password'], results[0]['password'], function(err, res) {
+        if(err) {
+          socket.emit('form-response', {form:'login-error', message:'There was a hashing error. Try again later.'});
+          return;
+        }
+        if(!res) {
+          socket.emit('form-response', {form:'login-error', message:'The password you entered is incorrect.'});
+          return;
+        }
+        sockets[clientId]['cookies']['hawkchat-loggedin-identifier'] = shortid.generate() + shortid.generate() + shortid.generate();
+        sockets[clientId]['loggedIn'] = true;
+        socket.emit('cookie-create', sockets[clientId]['cookies']);
+        results[0]['password'] = null;
+        loggedUsers[sockets[clientId]['cookies']['hawkchat-loggedin-identifier']] = results[0];
+        socket.emit('http-redirect', {page:'/webpanel.html', time:1000});
+        socket.emit('form-response', {form:'login-error', message:'You have succesfully logged in. Redirecting...'});
       });
     });
   });
-  /* signup-btn */
+  /* login-btn */
 });
--- public/utility/hawkchat.js
+++ public/utility/hawkchat.js
@@ -1,18 +1,73 @@
 var socket = io();
-
 /* clientId */
 var clientId;
 socket.on('clientId', function($clientId) {
 	clientId = $clientId;
 });
 /* clientId */
-/* loginId */
-var loginId;
-/* loginId */
+/* cookie */
+socket.on('cookie-create', function($cookies) {
+	var cookieString = "";
+	for(var cookie in $cookies) {
+		var cookieKey = cookie;
+		var cookieValue = $cookies[cookie];
+		cookieString = cookieString + cookieKey + "=" + cookieValue + ";";
+	}
+	document.cookie = cookieString;
+});
+socket.on('cookie-destroy', function() {
+	destroyCookies();
+});
+function destroyCookies() {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
+}
+function getCookie($cookieName) {
+	var cookies = document.cookie.split(";");
+	for($cookie in cookies) {
+		if(cookies[$cookie].indexOf($cookieName) != -1) {
+			return cookies[$cookie].split("=")[1];
+		}
+	}
+}
+/* cookie */
+/* login manager */
+var userData;
+if(window.location.href.indexOf("webpanel") != -1) {
+	if(document.cookie.indexOf("hawkchat-loggedin-identifier") == -1) {
+		window.location.replace('/login.html');
+	} else {
+		/* verify-hawkchat-loggedin-identifier */
+		socket.on('verify-hawkchat-loggedin-identifier', function($userData) {
+			if($userData == null) {
+				destroyCookies();
+				window.location.replace('/login.html');
+			} else {
+				userData = $userData;
+				$('#navbar-logout-a').click(function() {
+					destroyCookies();
+					window.location.replace('/index.html');
+				});
+			}
+		});
+		var loggedInIdentifier = getCookie('hawkchat-loggedin-identifier');
+		socket.emit('verify-hawkchat-loggedin-identifier', loggedInIdentifier);
+		/* verify-hawkchat-loggedin-identifier */
+	}
+}
+if(window.location.href.indexOf("signup") != -1) {
+	destroyCookies();
+}
+/* login manager */
 /*  form-response */
 socket.on('form-response', function($data) {
-	$('#signup-error').html($data['message']);
-	$('#signup-error').removeClass('hide');
+	$('#' + $data['form']).html($data['message']);
+	$('#' + $data['form']).removeClass('hide');
 });
 /* form-response */
 /* signup-btn */
@@ -20,3 +75,15 @@ $('#signup-btn').click(function() {
 	socket.emit('signup-btn', {email:$('#signup-email').val(), password:$('#signup-password').val(), affiliation:$('#signup-affiliation').val()})
 });
 /* signup-btn */
+/* login-btn */
+$('#login-btn').click(function() {
+	socket.emit('login-btn', {email:$('#login-email').val(), password:$('#login-password').val()});
+});
+/* login-btn */
+/* http-redirect */
+socket.on('http-redirect', function($data) {
+	setTimeout(function() {
+		window.location.replace($data['page']);
+	}, $data['time']);
+});
+/* http-direct */
